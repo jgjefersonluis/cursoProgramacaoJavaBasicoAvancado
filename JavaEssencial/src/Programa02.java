15f75493f9f42d3c21033c01f42dae2889ea88bf
Jeferson Luis <jgjefersonluis@gmail.com>
2020-04-14 05:24:54 -0300

    compilador, bytecode, variável, println e console

---
+++ JavaEssencial/src/Programa02.java
@@ -0,0 +1,15 @@
+
+public class Programa02 {
+
+	public static void main(String[] args) {
+		// Declarando e inicializando variável
+		int idade = 48;
+
+		System.out.println("Eu tenho, " + idade + " anos.");
+		
+		// javac Programa.java -> Programa.class -> java Programa
+		// codigo fonte (Programa.java-> bytecode (Programa.class) -> executável
+
+	}
+
+}
