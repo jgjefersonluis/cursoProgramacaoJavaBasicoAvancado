7a013fb29eee9f27ec7bb18b46a566150a5177b8
Jeferson Luis <jgjefersonluis@gmail.com>
2020-04-16 20:42:43 -0300

    tipos boolean

---
+++ JavaEssencial/src/Programa14.java
@@ -0,0 +1,44 @@
+//Tipos booleanos
+public class Programa14 {
+
+	public static void main(String[] args) {
+		// tipos primitivos
+		boolean verdadeiro = true;
+		boolean falso = false;
+
+		// tipos nao primitivos
+		Boolean v = true;
+		Boolean f = false;
+
+		boolean ativo = true; // boolean ativo = false;
+
+		System.out.println("Verdadeiro" + verdadeiro);
+		System.out.println("Falso" + falso);
+		System.out.println("V" + v);
+		System.out.println("F" + f);
+
+		if (verdadeiro) {
+			System.out.println("É Verdadeiro");
+		} else {
+			System.out.println("É Falso");
+		}
+
+		if (1 == 3) {
+			System.out.println("1 == 1 é Verdadeiro");
+		} else {
+			System.out.println("... é falso");
+		}
+
+		if (ativo) {
+			System.out.println("Pode acessar o sistema.");
+		} else {
+			System.out.println("Acesso negado!");
+		}
+
+	}
+
+}
+
+/*
+ * if(realiza uma verificação que no final retorna ou true ou false
+ */
