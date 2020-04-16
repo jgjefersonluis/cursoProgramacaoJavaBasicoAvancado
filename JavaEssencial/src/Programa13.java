e9c9dddac5a6ee9413f0419fd8e74e0241904765
Jeferson Luis <jgjefersonluis@gmail.com>
2020-04-16 19:59:34 -0300

    tipos alfanumericos char e strings

---
+++ JavaEssencial/src/Programa13.java
@@ -0,0 +1,36 @@
+//Tipos de dados alfanuméricos
+//caracteres e strings
+public class Programa13 {
+
+	public static void main(String[] args) {
+		// Tipos primitivos
+		char letra1 = 'a'; // Aspas simples
+		char letra2 = 97; // 97 em decimal == 'a'
+		// char letra4 = "b"; // Errado
+
+		System.out.println("Letra2 " + letra2);
+
+		letra2 = (char) (letra2 + 1); // cast
+
+		System.out.println("Letra1 " + letra1);
+		System.out.println("Letra2 " + letra2);
+
+		// Tipos não primitivos
+		Character letra3 = 'A';
+		String nome = "Geek University";
+
+		System.out.println("Letra3 " + letra3);
+
+		System.out.println("char/Character " + Character.SIZE + " bits");
+
+		System.out.println("Valor Min char/Charcter " + Character.MIN_VALUE);
+		System.out.println("Valor Max char/Charcter " + Character.MAX_VALUE);
+
+		System.out.println("Nome " + nome);
+
+		System.out.println("Tamanho da String: " + nome.length());
+
+		System.out.println("String " + (Character.SIZE + nome.length()) + " bits.");
+	}
+
+}
