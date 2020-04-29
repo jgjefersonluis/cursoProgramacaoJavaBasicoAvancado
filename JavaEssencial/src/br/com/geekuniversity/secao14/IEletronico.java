75e9bc8404e833bf0706b4248002cd0f2959ce60
Jeferson Luis <jgjefersonluis@gmail.com>
2020-04-29 01:53:44 -0300

    Secao 14Interfaces

---
+++ JavaEssencial/src/br/com/geekuniversity/secao14/IEletronico.java
@@ -0,0 +1,20 @@
+package br.com.geekuniversity.secao14;
+/*
+ * Uma interface pode conter:
+ * - constantes
+ * - metodos abstratos;
+ * 
+ * Interface para servir de contrato para produtos eletronicos. Todo
+ * produto eletronico que implementar esta interface OBRIGATORIAMENTE
+ * deverao implementar os metodos abstratos.
+ * 
+ */
+
+public interface IEletronico {
+
+	public String MARCA = "Geek";
+
+	public void ligar();
+
+	public void desligar();
+}
